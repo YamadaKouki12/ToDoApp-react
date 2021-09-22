e9c533f17706713fa455ff377bfbffcbdd7f2d82
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-23 00:20:00 +0900

    add: 削除機能

--- src/App.js
+++ src/App.js
@@ -20,6 +20,18 @@ const App = () => {
     setTodos(todos.filter((todo,index) => (index!==key)));
   }
 
+  const deleteTodo = (key) => {
+    const newTodos=[...todos];
+    newTodos.splice(key,1);
+    setTodos(newTodos);
+  }
+
+  const deleteDone = (key) => {
+    const newDones=[...dones];
+    newDones.splice(key,1);
+    setDones(newDones);
+  }
+
   return(
     <div>
       <h2>TODOアプリ</h2>
@@ -28,13 +40,20 @@ const App = () => {
       <h3>することリスト</h3>
       <ol>  
         {todos.map((todo,index) => (
-          <li key={index}>{todo} <button onClick={() => makeDone(index)}>Done</button> </li>
+          <li key={index}>
+            {todo}
+            <button onClick={() => makeDone(index)}>Done</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
         ))}
       </ol>
       <h3>終わったことリスト</h3>
       <ol>  
         {dones.map((done,index) => (
-          <li key={index}>{done}</li>
+          <li key={index}>
+            {done}
+            <button onClick={() => deleteDone(index)}>Delete</button>
+        </li>
         ))}
       </ol>
     </div>
