daee3b2d464cd69d055b6cd46882dca9ce621b21
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-22 21:34:28 +0900

    add: todoをdoneに

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 
 const App = () => {
   const [input,setInput] = React.useState('');
-  const [todos,setTodos] = React.useState(['a','b'])
-  const [dones, setDones] = React.useState(['c','d']);
+  const [todos,setTodos] = React.useState(['a','b','c'])
+  const [dones, setDones] = React.useState(['d','e']);
 
   const handleChange = (event) => {
     setInput(event.target.value);
@@ -15,17 +14,22 @@ const App = () => {
     setTodos([...todos,input]);
     setInput('');
   }
+
+  const makeDone = (key) => {
+    setDones([...dones,todos[key]])
+    setTodos(todos.filter((todo,index) => (index!==key)));
+  }
+
   return(
     <div>
       <h2>TODOアプリ</h2>
       <input type="text" value={input} onChange={handleChange} placeholder='タスクを入力してください'/>
-      <Button onclick={addTodo} variant="contained">Add</Button>
+      <button onClick={addTodo}>Add</button>
       <h3>することリスト</h3>
       <ol>  
         {todos.map((todo,index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>{todo} <button onClick={() => makeDone(index)}>Done</button> </li>
         ))}
-        
       </ol>
       <h3>終わったことリスト</h3>
       <ol>  
