7f80ca8979d085d2cf521003a7d185558f41533e
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-22 20:48:41 +0900

    update: show todos and dones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const App = () => {
+  const [input,setInput] = React.useState('');
+  const [todos,setTodos] = React.useState(['a','b'])
+  const [dones, setDones] = React.useState(['c','d']);
+
+  
   return(
     <div>
       <h2>TODOアプリ</h2>
+      
+      <h3>することリスト</h3>
+      <ol>  
+        {todos.map((todo,index) => (
+          <li key={index}>{todo}</li>
+        ))}
+        
+      </ol>
+      <h3>終わったことリスト</h3>
+      <ol>  
+        {dones.map((done,index) => (
+          <li key={index}>{done}</li>
+        ))}
+      </ol>
     </div>
   )
 }
